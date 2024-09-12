d57819a53f2f0466e350ee0dd869510c67104d46
IgorAlexandre43 <168492143+IgorAlexandre43@users.noreply.github.com>
2024-09-12 19:54:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha do skate, é uma skatista brasileira que conquistou o mundo com seu talento e carisma, tornando-se a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate sk8 fada"
+    },
+    {  
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca no cenário nacional e internacional, representando o Brasil em diversas competições e inspirando novas gerações.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" ,
+        tags:"judo judô judoka"
+    },   
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes da ginástica artística mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginastica ginástica ouro"
+    },
+]
